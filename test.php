2afac77bb4db5585bba3b3ae930cd26ff3d9181b
dainmas <dainorai@inbox.lt>
2019-09-09 11:31:13 +0300

    Papildymas

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<html>
+    <head>
+        <title>Aš valdau HTML</title>
+    </head>
+    <body>
+        <h1>Dainora - HTML asė</h1>
+        <p>Moku kurti statinį turinį</p>
+    </body>
+</html>
