63bd6b7f3d1b7991683eb21f7459c204b391f61a
dainmas <dainorai@inbox.lt>
2019-09-09 12:18:43 +0300

    papildyta php

--- test.php
+++ test.php
@@ -1,18 +1,12 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 ?>
 <html>
     <head>
-        <title>Aš valdau HTML</title>
+        <title>Aš valdau HTML<?php print 'ir PHP';?></title>
     </head>
     <body>
-        <h1>Dainora - HTML asė</h1>
-        <p>Moku kurti statinį turinį</p>
+        <h1>Dainora - HTML asė <?php print 'ir PHP';?></h1>
+        <p>Moku kurti statinį turinį. Atidaryta <?php print date('Y-m-d h:i:sa');?></p>
     </body>
 </html>
